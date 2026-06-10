3b8e1bcaeb94382f9a0b22cd3a3edc7cbcef4e04
SrHenry <lucapvh949@gmail.com>
2026-06-09 22:15:57 -0400

    refactor(shared): add shared type modules

---
+++ src/shared/types/index.ts
@@ -0,0 +1,21 @@
+export type { ValidInput, Input } from './Input'
+
+export type {
+    FileStreamMode,
+    FileStreamOptions,
+    FileStreamType,
+    FileStreamOptionsType,
+    NodeJS_fsReadOptions,
+    NodeJS_fsWriteOptions,
+    NodeJS_fsDuplexOptions,
+    NodeJS_fsOptionsType,
+    ReadMode,
+    WriteMode,
+    DuplexMode,
+} from './FileStreamMode'
+
+export type {
+    RecursiveType,
+    DirectoryListJSONContent,
+    DirectoryListJSON,
+} from './DirectoryListTypes'
